1ec8e27597b40178bfd418ab5216dd53c6afb76e
SheneB07 <shenebanga@gmail.com>
2024-11-27 09:43:39 +0100

    Project1

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+    public float damage = 50f;
+    private Transform target;
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, target.position) < 0.2f)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        // Zoek Health component van target met GetComponent
+        // Als Health script gevonden is, gebruik TakeDamage functie
+        // Gebruik damage variable
+        // Instantiate eventuele effecten
+
+        Destroy(gameObject);
+    }
+
+}
+}
